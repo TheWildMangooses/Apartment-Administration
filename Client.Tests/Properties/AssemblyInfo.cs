5c75a2468968ba74fe51b119f7ccb9b3f4524012
adi97ida <adi97ida@gmail.com>
2017-05-31 08:41:15 +0200

    final changes for the moment, more will come

---
+++ Client.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Client.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Client.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("bdd5437e-13da-4d8c-be9f-10db8c455375")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
--- Client/Model/ResidentModel.cs
+++ Client/Model/ResidentModel.cs
@@ -39,6 +39,5 @@ namespace Client.Model
         #endregion
 
 
-
     }
 }
